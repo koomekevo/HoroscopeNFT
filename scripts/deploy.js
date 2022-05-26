4df3970d6ff4e133433f5536429999b552f1e293
koomekevo <koome.kevo@gurucrafts.net>
2022-05-26 23:39:35 +0300

    Modify hardhat.config.js and deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,19 +2,22 @@ const hre = require("hardhat");
 
 async function main() {  
 
-    // We get the contract to deploy  
     const horoscopeNFT = await hre.ethers.getContractFactory("horoscopeNFT");
     const hscp = await horoscopeNFT.deploy();   
     await hscp.deployed();
-    
+	
+	//since we are testing, you should mention your own Eth wallet address
+    const myAddress="0xb2b11f72d881FC0eC2c02e68f898D6ab661a0634";
     console.log("horoscopeNFT deployed to:", hscp.address);   
+
+    let txn = await hscp.mintNFT(myAddress, 'virgo');
+    await txn.wait();
+
  } 
- 
-// We recommend this pattern to be able to use async/await
-//everywhere and properly handle errors.
+
 main()
     .then(() => process.exit(0))  
     .catch((error) => {    
     console.error(error);
-    process.exit(1);  
-});
+    process.exit(1); 
+ });
